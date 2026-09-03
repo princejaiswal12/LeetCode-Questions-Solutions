4758a5d83ba7bd1eb57b80433422392c29f8a9bf
Prince Jaiswal <149855621+princejaiswal12@users.noreply.github.com>
2026-09-03 22:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.74%) - LeetSync

---
+++ 3567-convert-date-to-binary/convert-date-to-binary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string toBinary(int n) {
+        string res;
+        
+        while (n > 0) {
+            res += char('0' + (n % 2));
+            n /= 2;
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
+    string convertDateToBinary(string date) {
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8, 2));
+
+        return toBinary(year) + "-" + 
+               toBinary(month) + "-" + 
+               toBinary(day);
+    }
+};
