3a1bb539666fa2e136734fea3d499b333396e292
Prince Jaiswal <149855621+princejaiswal12@users.noreply.github.com>
2026-09-10 08:29:29 +0530

    Time: 3 ms (69.79%) | Memory: 16.8 MB (62.50%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> mp;
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < groupSizes.size(); i++) {
+            int size = groupSizes[i];
+
+            mp[size].push_back(i);
+
+            // Group is complete
+            if (mp[size].size() == size) {
+                ans.push_back(mp[size]);
+                mp[size].clear();
+            }
+        }
+
+        return ans;
+    }
+};
