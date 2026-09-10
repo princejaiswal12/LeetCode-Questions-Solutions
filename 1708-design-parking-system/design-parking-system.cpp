51ed9d5d9da86a5d0785e07a2dfe39ff53c1e590
Prince Jaiswal <149855621+princejaiswal12@users.noreply.github.com>
2026-09-10 13:36:03 +0530

    Time: 6 ms (57.02%) | Memory: 39 MB (2.30%) - LeetSync

---
+++ 1708-design-parking-system/design-parking-system.cpp
@@ -0,0 +1,22 @@
+class ParkingSystem {
+public:
+    unordered_map<int,int> m;
+    ParkingSystem(int big, int medium, int small) {
+        m[1] = big;
+        m[2] = medium;
+        m[3] = small;
+    }
+    
+    bool addCar(int carType) {
+        if(m[carType]){
+            m[carType]--;
+            return true;
+        }else return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
